da96197ca3659b22af44b9c3108d6be190d1a896
Daniel Thor Thorisson <danielthorr@gmail.com>
2019-02-27 00:39:36 +0000

    Folder structure and comments
    Fixed the folder structure. The real project was in the wrong folder.
    Commented a lot of code and cleaned it up a lot.

--- Midannarverkefni/client.py
+++ Midannarverkefni/client.py
@@ -7,23 +7,24 @@ hostName = socket.gethostbyname(socket.gethostname())
 
 port = 9999
 
+# The hangman class mostly serves to send to the server and to switch frames
 class Hangman(tk.Tk):
     def __init__(self):
         tk.Tk.__init__(self)
         self.frame = None
         self.switchFrame(StartFrame)
 
-    def Update(self, data):
-        command, content = data.split(";")
-
+    # All these different send commands just take in a value from our GameFrame class
+    # and forward them to the server
     def sendDifficulty(self, diff):
-        clientSocket.sendall(bytes("diff;"+diff, "utf8"))
+        clientSocket.sendall(bytes("difficulty;"+diff, "utf8"))
         self.switchFrame(GameFrame)
 
     def sendGuess(self, guess):
         clientSocket.sendall(bytes("guess;"+guess, "utf8"))
         self.switchFrame(GameFrame)
 
+    # This method switches frames to and from the start menu, options and game
     def switchFrame(self, frameClass):
         # Destroys current frame and replaces it with a new one.
         newFrame = frameClass(self)
@@ -32,90 +33,93 @@ class Hangman(tk.Tk):
         self.frame = newFrame
         self.frame.pack()
     
+    # This method simply sends a disconnect message to the server
+    # then destroys itself
     def exitGame(self):
+        clientSocket.sendall(b"disconnecting;")
         self.destroy()
 
+# Start frame presents you with two buttons, very simple
 class StartFrame(tk.Frame):
     def __init__(self, master):
-        self.width = 400
-        self.height = 2505
         tk.Frame.__init__(self, master)
         tk.Label(self, text="This is the start page").pack(side="top", fill="x", pady=10)
         tk.Button(self, text="Start", command=lambda: master.switchFrame(SetupFrame)).pack()
         tk.Button(self, text="Quit", command=lambda: master.exitGame()).pack()
 
+# The setupframe class gives us options for the difficulty setting
 class SetupFrame(tk.Frame):
     def __init__(self, master):
         tk.Frame.__init__(self, master)
         tk.Label(self, text="Let's play hangman, choose a difficulty").pack(side="top", fill="x", pady=10)
-        #tk.Label(self, text="This is page one").pack(side="top", fill="x", pady=10)
+
+        # These next three buttons trigger the Hangman class' sendDifficulty method
         tk.Button(self, text="Easy", command=lambda: master.sendDifficulty("easy")).pack()
         tk.Button(self, text="Medium", command=lambda: master.sendDifficulty("medium")).pack()
         tk.Button(self, text="Hard", command=lambda: master.sendDifficulty("hard")).pack()
+
+        # This button simply goes back to the main menu
         tk.Button(self, text="Back to main menu", command=lambda: master.switchFrame(StartFrame)).pack()
 
+# The gameframe class is our main class, it displays everything we need to know during play
 class GameFrame(tk.Frame):
     def __init__(self, master):
         tk.Frame.__init__(self, master)
 
-        try:
-            data = clientSocket.recv(1024).decode("utf8")
-            command, content = data.split(";")
-            content = content.split(":")
-        except:
-            pass
-            
+        # Here we get data from our server and split it into usable chunks
+        data = clientSocket.recv(1024).decode("utf8")
+        # The command part, seperated by a semicomma is unused in this case
+        command, content = data.split(";")
+        # The content part is split into a list of messages
+        content = content.split(":")
+        
         tk.Label(self, text="Welcome to hangman").grid(row=0)
+
+        # Here we use display the main message the game sends to us
+        # those are things such as victory or failure and more
         tk.Label(self, text=content[0]).grid(row=0, column=1)
+
+        # We have a list of names that we will use to show
+        # the status of our hanging man
         self.images = [
             "hman0", "hman1", "hman2", "hman3", 
             "hman4", "hman5", "hman6", "hman7", 
             "hman8", "hman9", "hman10"
             ]
+        # Instantiate a list
         self.hman = []
+        # We iterate through the images in our images list
+        # and load them into our hman list
         for i in range(len(self.images)):
             img = tk.PhotoImage(file=self.images[i] + ".png")
             self.hman.append(tk.Label(self, image=img))
             self.hman[i].image = img
             
+        # We check how many lives we have left (content[1]) to
+        # see which image we should display
         self.hman[ (10-int(content[1])) ].grid(row=1, column=0)
         
+        # Display the letters the player has already guessed
         tk.Label(self, text="Letters guessed: " + content[2]).grid(row=1, column=1)
+        # Display the progress of the word to be guessed
         tk.Label(self, text="Word progress: " + content[3]).grid(row=2, column=1)
 
+        # Here we get input from the player
         self.e = tk.Entry(self)
         self.e.grid(row=3, column=0)
 
+        # With the check button we trigger Hangman class' method sendGuess, 
+        # passing in the player's guess
         tk.Button(self, text="Check", command=lambda: master.sendGuess(self.e.get())).grid(row=3, column=1)
-
-        #tk.Label(image=photo).pack()
         
-        #tk.Button(self, text="Return to start page", command=lambda: master.switchFrame(StartFrame)).grid(row=4)
-        tk.Button(self, text="Return to start page", command=lambda: send("SomeText")).grid(row=4)
-
-data = "default"
-from sysconfig import sys
-def receive():
-    print("checking socket...")
-
-    while True:
-        try:
-            data = clientSocket.recv(1024).decode("utf8")
-            data = tk.StringVar
-            #print("Client received: " + data)
-        except OSError:
-            break
-
-def send(thingToSend):
-    clientSocket.sendall(bytes(thingToSend, "utf8"))
-        
+# Open a socket to communicate with our server
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as clientSocket:
+    # Try to connect
     clientSocket.connect((hostName, port))
 
+    # Send a message to confirm that we are ready
     clientSocket.sendall(b"ready;")
 
-    #receiveThread = threading.Thread(target=receive)
-    #receiveThread.start()
-
+    # Activate the tkinter loop
     app = Hangman()
     app.mainloop()
--- Midannarverkefni/server.py
+++ Midannarverkefni/server.py
@@ -14,8 +14,10 @@ except socket.gaierror:
 # Establish a port to be used on both the server and client
 port = 9999
 
+# We create a class for our player and their values
 class PlayerManager:
     def __init__(self):
+        # Initiate some variables
         self.message = "Have fun"
 
         self.easyWords = ["glass", "tree", "swing", "house", "bright", "clean", "trim", "glue"]
@@ -32,25 +34,28 @@ class PlayerManager:
         self.lettersGuessed = []
         self.revealed = ""
 
+        # If the player has won or lost the game is stopped
+        self.stop = False
+
+    # Get the player's preferred difficulty
     def setDifficulty(self, diff):
         if (diff == "easy"):
-            self.difficulty = 0
             self.wordToGuess = self.easyWords[randint(0, len(self.easyWords)-1)]
         if (diff == "medium"):
-            self.difficulty = 1
             self.wordToGuess = self.mediumWords[randint(0, len(self.mediumWords)-1)]
         if (diff == "hard"):
-            self.difficulty = 2
             self.wordToGuess = self.hardWords[randint(0, len(self.hardWords)-1)]
 
-        self.wordLength = len(self.wordToGuess)
-
+        # Here we fill a string with either blanks or the words
+        # that the player has guessed correctly
         for i in range(len(self.wordToGuess)):
                 self.revealed += " _ "
 
         print("difficulty is: " + str(self.difficulty))
         print("word to guess: " + self.wordToGuess)
 
+    # This is used by the guessCheck method
+    # to update the revealed word
     def wordUpdate(self, guess):
         self.revealed = ""
         for i in range(len(self.wordToGuess)):
@@ -59,38 +64,60 @@ class PlayerManager:
             else:
                 self.revealed += " _ "
 
+    # Here we check what the player guessed
     def guessCheck(self, guess):
+        # We assign it to lowercase so it doesn't become an issue
         guess = guess.lower()
+        # Cancel is used to check if the player wrote
+        # too many letters or has guessed the letter
         cancel = False
+        # We wrap everything in one if statement that will not
+        # run if the player has either won or lost
+        if not self.stop:
+            # If the player put too few or too many characters
+            if len(guess) != 1:
+                self.message = "Make sure to only write one letter"
+                cancel = True
 
-        if len(guess) != 1:
-            self.message = "Make sure to only write one letter"
-            cancel = True
+            # If the player has already guessed the letter
+            if guess in self.lettersGuessed:
+                self.message = "You've already guessed this letter"
+                cancel = True
 
-        if guess in self.lettersGuessed:
-            self.message = "You've already guessed this letter"
-            cancel = True
+            if not cancel:
+                # Check if the guess was correct and either
+                # withdraw a life point or add 1 to correct guesses
+                if guess not in self.wordToGuess:
+                    self.lives -= 1
+                    self.message = guess + " is incorrect"
+                else:
+                    self.message = guess + " is correct"
+                    for char in self.wordToGuess:
+                        if guess == char:
+                            self.correctLetters += 1
 
-        if not cancel:
-            if guess not in self.wordToGuess:
-                self.lives -= 1
-                self.message = guess + " is incorrect"
-            else:
-                self.message = guess + " is correct"
-                for char in self.wordToGuess:
-                    if guess == char:
-                        self.correctLetters += 1
-            self.lettersGuessed.append(guess)
-            self.wordUpdate(guess)
+                # Append the guessed letter to the list of letters guessed
+                self.lettersGuessed.append(guess)
+                # Update the revealed word to reflect the progress made
+                self.wordUpdate(guess)
 
-            if self.correctLetters >= len(self.wordToGuess):
-                self.message = "Congratulations! You won!"
-            elif self.lives <= 0:
-                self.message = "I'm sorry, you lost. The word was \"" + self.wordToGuess + "\""
-
+                # If the number of correct letters is the same or higher
+                # than the word to guess, the player has won
+                if self.correctLetters >= len(self.wordToGuess):
+                    self.stop = True
+                    self.message = "Congratulations! You won!"
+                # If the player has run out of lives they have lost
+                elif self.lives <= 0:
+                    self.stop = True
+                    self.message = "I'm sorry, you lost. The word was \"" + self.wordToGuess + "\""
+        
+        # When we've done all of our checking we call our send method
+        # to send the results to our player
         self.send(self.message)
 
+    # Here we send the player their updated progress
     def send(self, msg):
+        # Create a string to send all the letters that the player has guessed so far
         guessLettersSend = " "
         for letter in self.lettersGuessed:
             guessLettersSend += letter + ", "
@@ -117,34 +144,25 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as serverSocket:
             # We receive data in bytes, decode them to the utf8 standard
             data = conn.recv(1024).decode("utf8")
 
+            # We split the data into command and content
+            # We use the command to determine what step to take
             command, content = data.split(";")
-
-            print(content)
 
             if command == "ready":
                 print("Client ready")
                 conn.sendto(bytes("If you see this it's working", "utf8"), addr)
                 player.send("Have fun!")
-            if command == "gameready":
-                print("received stuff")
-                conn.sendto(bytes("Received the stuff", "utf8"), addr)
 
             if command == "guess":
                 print("checking guess")
                 player.guessCheck(content)
 
-            if command == "diff":
+            if command == "difficulty":
                 print("received difficulty")
                 player.setDifficulty(content)
 
             # The client sends this message if it's ready to disconnect
-            if data == "disconnecting":
+            if command == "disconnecting":
                 print("client is disconnecting")
                 # This break statements breaks our while loop which keeps the connection open
                 break
-
-#   --------    Extra information   --------    #
-#   bind() is used to associate the socket with a specific network interface and port number.
-#   The values passed to bind() depend on the address family of the socket. In this example, socket.AF_INET expects a 2-tuple: (host, port).
-#   host can be a hostname, IP address, or empty string. If an IP address is used, it should be IPv4.1 27.0.0.1 is the loopback interface, so only processes on the host will be able to connect to the server. If you pass an empty string, the server will accept connections on all available IPv4 interfaces.
-#   port should be an integer from 1-65535 (0 is reserved). It’s the TCP port number to accept connections on from clients. Some systems may require superuser privileges if the port is < 1024.
